da536e278caa1aba4a5bd8c4ef2586672eb5f4a1
cynthia Amarachi <ceeyntia@gmail.com>
2024-03-12 07:15:58 -0700

    fixed responsiveness

--- js/index.js
+++ js/index.js
@@ -19,3 +19,45 @@ function mapFunction(item) {
 }
 const  newValue = hdsStudents.map(mapFunction)
 console.log(newValue)
+const food=['rice','beans']
+// const newFood = food.map((name)=>({name,status:'available'}))
+// console.log(newFood)
+const studentArr=[
+    {
+        name:'mary',
+        score:20
+    },
+    {
+        name:'larry',
+        score:10
+    },
+    {
+        name:'fary',
+        score:60
+    },
+    {
+        name:'jerry',
+        score:50
+    }
+]
+//ternary operator
+let today='wenesday'
+today==='wednesday'?console.log('yes'):console.log('no')
+//we are calculating the student  perrformance based on the next student in the list
+// so we are comparing the current student score to the score of the next student
+// and to do this we take the current student score and divide it by the next student score then multiplied by 100
+const resultArr= studentArr.map((student,index,students)=>{
+    // let performance =0;
+    // if(students[index +1]){ //checks if theres a next student in the array
+    //     performance = (student.score/students[index +1].score) *100;//calculate the current  score based on the next student score
+    // }
+    const performance = (student.score/(students[index +1]?.score)??0) *100 //optional chaining to check if student score exist and assign score 0 to it if it dosent 
+    return {
+        name: student.name,
+        age: student?.age, // optional chaining here to check if age exists
+        score: student.score,
+        performance: performance,
+        index: index
+    };
+})
+console.log(resultArr)
